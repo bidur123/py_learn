f8dc81bd15b8af3166ba65cae06cea199f85a0f5
bidur123 <48418967+bidur123@users.noreply.github.com>
2020-05-07 22:38:53 -0500

    minor_changes

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
 #       backend "remote" {
 #         # The name of your Terraform Cloud organization.
-#         organization = "example-organization"
+#         organization = "bidur_home"
 #
 #         # The name of the Terraform Cloud workspace to store Terraform state files in.
 #         workspaces {
-#           name = "example-workspace"
+#           name = "home_develop"
 #         }
 #       }
 #     }
